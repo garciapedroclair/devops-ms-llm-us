6d3a36583107873632658cb90ca412e0dd13cdc3
pedroclair <pedro.clair@aluno.ufop.edu.br>
2025-12-30 12:23:44 -0300

    Refactor main.py to include database connection setup and add endpoints for health check, participants, and tasks

--- server/main.py
+++ server/main.py
@@ -1,11 +1,22 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy import create_engine, text
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
 app = FastAPI(title="Message Microservice")
 
 origins = [
     "http://localhost:5173",  # React dev server
 ]
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL, echo=False)
+SessionLocal = sessionmaker(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
@@ -17,4 +28,31 @@ app.add_middleware(
 
 @app.get("/health")
 def health_check():
-    return {"status": "ok"}
+    db = SessionLocal()
+    result = db.execute(text("""
+            SELECT table_name
+            FROM information_schema.tables
+            WHERE table_schema = 'public'
+            ORDER BY table_name;
+        """))
+    tables = [row[0] for row in result.fetchall()]
+    db.close()
+    return {"tables": tables}
+
+@app.get("/participants")
+def list_participants_atributes():
+    db = SessionLocal()
+    query = db.execute(text("Select * From participant"))
+    columns =  query.keys()
+    data = [dict(zip(columns, row)) for row in query.fetchall()]
+    return data
+
+@app.get("/tasks")
+def list_tasks_attributes():
+    db = SessionLocal()
+    query = db.execute(text("SELECT * FROM task"))
+    columns = query.keys()
+    data = [dict(zip(columns, row)) for row in query.fetchall()]
+    db.close()
+    return data
+
