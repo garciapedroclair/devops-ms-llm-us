259a4924c11dee96113d371731716a924e601d0a
pedroclair <pedro.clair@aluno.ufop.edu.br>
2025-12-30 07:16:14 -0300

    Refactor API and add message schemas for creation and response handling

--- server/main.py
+++ server/main.py
@@ -1,16 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 from dotenv import load_dotenv
-import os
+
+from database import engine
+from dependencies import get_db
+from models import Base
+from repositories.message_repository import (
+    create_message,
+    list_messages
+)
+from schemas.message import (
+    MessageCreate,
+    MessageResponse
+)
 
 load_dotenv()
 
 app = FastAPI(title="Message Microservice")
 
+# DEV ONLY: create tables automatically
+Base.metadata.create_all(bind=engine)
+
 @app.get("/health")
 def health_check():
-    return {
-        "status": "ok",
-        "database_url_loaded": bool(os.getenv("DATABASE_URL"))
-    }
+    return {"status": "ok"}
 
-# Dependences implemented
+@app.post("/messages", response_model=MessageResponse)
+def create_message_endpoint(
+    payload: MessageCreate,
+    db: Session = Depends(get_db)
+):
+    return create_message(db, payload.content)
+
+@app.get("/messages", response_model=list[MessageResponse])
+def list_messages_endpoint(
+    db: Session = Depends(get_db)
+):
+    return list_messages(db)
